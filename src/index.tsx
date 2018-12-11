5bfa4f03134ecfbb23768c1e4c801441f9ec4f1e
forsigner <forsigner@gmail.com>
2018-12-11 17:39:22 +0800

    feat: can useQuery

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
-console.log('foo')
+import { createStore } from 'stamen'
+import { query } from 'gery'
+import gql from 'gql-tag'
+import { useQuery, usePull } from 'request-hooks'
+
+import { dahlia } from './dahlia'
+
+export default dahlia
+export { gql, query, useQuery, usePull, createStore }
