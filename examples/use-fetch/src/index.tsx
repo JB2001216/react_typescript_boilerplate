2df7f08671af3d7a62c85ad8c2b91df92a28016b
JB2001216 <zhangxing2002611@gmail.com>
2018-12-12 16:10:35 +0800

    chore: add example use-fetch

---
+++ examples/use-fetch/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import dahlia, { useFetch } from 'dahlia'
+
+dahlia.init({
+  rest: {
+    endpoint: 'https://jsonplaceholder.typicode.com',
+  },
+})
+
+const App = () => {
+  const { loading, data, error } = useFetch('/todos/1')
+
+  if (loading) return <div>loading....</div>
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+
+  return (
+    <div className="App">
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
