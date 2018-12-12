113ea643e6609c4dbd8d84d51b05ae103126b70c
JB2001216 <zhangxing2002611@gmail.com>
2018-12-12 16:11:40 +0800

    refactor: dahlia -> Dahlia

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createStore } from 'stamen'
 import gql from 'gql-tag'
 import { useQuery, useMutate, useFetch, useUpdate } from 'request-hooks'
 
-import { dahlia } from './dahlia'
+import { Dahlia } from './dahlia'
 
-export default dahlia
+export default Dahlia
 export { gql, useQuery, useMutate, useFetch, useUpdate, createStore }
