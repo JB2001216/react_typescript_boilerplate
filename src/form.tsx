d2b4ce31e08afea305c9f0910032c138256ba948
JB2001216 <zhangxing2002611@gmail.com>
2019-01-10 11:16:53 +0800

    feat: form works

--- src/form.tsx
+++ src/form.tsx
@@ -1,1 +1,11 @@
-export const form = 11
+import { createForm } from 'fooks'
+import { check } from 'checkok'
+import string from 'checkok-string'
+import number from 'checkok-number'
+import required from 'checkok-required'
+import min from 'checkok-min'
+import max from 'checkok-max'
+import email from 'checkok-email'
+import url from 'checkok-url'
+
+export { createForm, check, string, number, required, min, max, email, url }
