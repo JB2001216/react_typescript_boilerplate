f314f128d0ad5aae807652bca884942961f0f919
JB2001216 <zhangxing2002611@gmail.com>
2019-03-09 16:16:23 +0800

    [dahlia-http] export dahliaHttpConfig

--- packages/dahlia-http/src/index.ts
+++ packages/dahlia-http/src/index.ts
@@ -1,8 +1,8 @@
 import { request } from './request'
-import { config } from './config'
 import { useQuery } from './use-query'
 import { useMutate } from './use-mutate'
 import { useFetch } from './use-fetch'
 import { useUpdate } from './use-update'
+import { config, dahliaHttpConfig } from './config'
 
-export { request, useQuery, useMutate, useFetch, useUpdate, config }
+export { request, useQuery, useMutate, useFetch, useUpdate, config, dahliaHttpConfig }
