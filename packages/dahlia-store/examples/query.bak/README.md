d01ad41fe7f0ced26c44cd054f1bc05415a00eb0
forsigner <forsigner@gmail.com>
2019-03-09 19:44:46 +0800

    [dahlia-store] improve example

--- packages/dahlia-store/examples/query.bak/README.md
+++
@@ -1,8 +0,0 @@
-## Stamen async example
-
-```bash
-git clone https://github.com/forsigner/stamen
-cd stamen/async
-yarn
-yarn start
-```
--- packages/dahlia-store/examples/query.bak/images.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
-
-declare module 'promise-hook' {
-  export function usePromise(a: any, b?: any): any
-}
