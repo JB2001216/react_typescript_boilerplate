c1dc18379ea9ecaca76328d8dd9e55c96296ae86
JB2001216 <zhangxing2002611@gmail.com>
2019-03-18 16:16:23 +0800

    [dahlia-store] use proxy

--- packages/dahlia-store/rollup.config.js
+++ packages/dahlia-store/rollup.config.js
@@ -1,9 +1,10 @@
 import typescript from 'rollup-plugin-typescript2'
+import autoExternal from 'rollup-plugin-auto-external'
 
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
@@ -18,8 +19,9 @@ export default {
       sourcemap: true,
     },
   ],
-  external: ['immer', 'fast-deep-equal', 'gery', 'cross-fetch', 'dahlia-http', 'react', 'react-dom'],
+  external: ['react', 'react-dom'],
   plugins: [
+    autoExternal(),
     typescript({
       rollupCommonJSResolveHack: true,
     }),
