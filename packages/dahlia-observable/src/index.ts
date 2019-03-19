801ceae547c7f55739c2586bb46f551b1aeeeab7
forsigner <forsigner@gmail.com>
2019-03-19 17:42:46 +0800

    [dahlia-observable] export raw

--- packages/dahlia-observable/src/index.ts
+++ packages/dahlia-observable/src/index.ts
@@ -1,5 +1,7 @@
 export { observable } from './api/observable'
 export { observe } from './api/observe'
 export { action } from './api/action'
+export { raw } from './api/raw'
+export { isObservable } from './api/isObservable'
 
 
