d3cc9e92d53909cfc08257819c825e041221487a
JB2001216 <zhangxing2002611@gmail.com>
2019-03-20 09:58:33 +0800

    [dahlia-router] upgrade dahlia-store

--- packages/dahlia-router/src/components/Router.tsx
+++ packages/dahlia-router/src/components/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Observer } from 'dahlia-store'
+import { observe } from 'dahlia-store'
 
 import { getPath, createPage, useMount, useUnmount } from '../util'
 import store from '../routerStore'
@@ -8,7 +8,7 @@ const handlePop = () => {
   store.go({ path: getPath(), replace: false })
 }
 
-const Router: any = (props: any) => {
+const Router = observe<{routes: any[]}>((props) => {
   const { currentPage, defaultPage, inited } = store
 
   useMount(() => {
@@ -26,8 +26,7 @@ const Router: any = (props: any) => {
   if (!currentPage) {
     return inited ? <DefaultPage /> : null
   }
-
-  return <Observer>{() => createPage([store.currentPage], [])}</Observer>
-}
+  return createPage([store.currentPage], [])
+})
 
 export default Router
