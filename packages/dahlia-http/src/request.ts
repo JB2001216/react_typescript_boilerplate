7f321ad1b45de90c2ff02af774c622bcf0bed2e5
JB2001216 <zhangxing2002611@gmail.com>
2019-03-21 22:15:35 +0800

    dahlia-http: set default content-type

--- packages/dahlia-http/src/request.ts
+++ packages/dahlia-http/src/request.ts
@@ -2,6 +2,29 @@ import fetch from 'cross-fetch'
 import { dahliaHttpConfig, Interceptor } from './config'
 import { Options } from './types'
 
+// default post with json
+const methods = ['POST', 'PUT', 'PATCH', 'DELETE']
+
+function getDefaultOpt(options: Options = {}): Options {
+  const baseOpt = { headers: {} } as Options
+  const { method } = options
+  if (method && methods.includes(method)) {
+    baseOpt.headers = {
+      'content-type': 'application/json; charset=utf-8',
+    }
+  }
+  return baseOpt
+}
+
+function getOpt(options?: Options) {
+  const defaultOpt = getDefaultOpt(options)
+
+  if (!options) return defaultOpt
+  const { headers } = options
+  options.headers = { ...defaultOpt.headers, ...headers }
+  return options
+}
+
 export const request = async <T extends any>(url: string, options?: Options): Promise<T> => {
   let reqURL: string = url
   let interceptors: Interceptor[] = []
@@ -16,9 +39,10 @@ export const request = async <T extends any>(url: string, options?: Options): Pr
     }
   }
 
-  const args: [string, Options?] = !options ? [reqURL] : [reqURL, options]
+  const opt = getOpt(options)
+
   try {
-    const res = await fetch(...args)
+    const res = await fetch(reqURL, opt)
     if (res.status >= 400) {
       throw new Error('Bad response from server')
     }
