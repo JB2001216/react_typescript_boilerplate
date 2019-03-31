fb28fe2d81308acd3f859658050e25782ff28139
forsigner <forsigner@gmail.com>
2019-03-31 20:56:51 +0800

    dahlia: export dahlia-modal

---
+++ packages/dahlia/modal.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var dahliaModal = require('dahlia-modal');
+
+
+
+Object.keys(dahliaModal).forEach(function (key) { exports[key] = dahliaModal[key]; });
+//# sourceMappingURL=modal.js.map
