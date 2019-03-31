4a8023171af635cea6f8faeb342e1061caa849ed
forsigner <forsigner@gmail.com>
2019-03-31 21:50:59 +0800

    dahlia-cli: fix texts

--- packages/dahlia-cli/src/utils/texts.ts
+++ packages/dahlia-cli/src/utils/texts.ts
@@ -24,7 +24,7 @@ const htmlText = formatCode(
 )
 
 export const entryText = formatCode(`
-import Dahlia, { Config } from '../src'
+import Dahlia, { Config } from 'dahlia'
 import routes from './config/router.config'
 import modals from './config/modal.config'
 
