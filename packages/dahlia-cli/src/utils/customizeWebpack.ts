a486fceabd6863144de38f1aaaeb711a05fab34b
JB2001216 <zhangxing2002611@gmail.com>
2019-04-04 14:21:24 +0800

    dahlia-cli: improve locales

--- packages/dahlia-cli/src/utils/customizeWebpack.ts
+++ packages/dahlia-cli/src/utils/customizeWebpack.ts
@@ -1,11 +1,17 @@
+import { join } from 'path'
 import override from 'dahlia-webpack-override'
 import styledJsx from 'dahlia-webpack-styled-jsx'
 import styleComponents from 'dahlia-webpack-styled-components'
+import alias from 'dahlia-webpack-alias'
 import { Configuration } from 'webpack'
 
-import { webpackConfigPath } from './paths'
+import { webpackConfigPath, appDir } from './paths'
 import { overrideWebpackExclude } from './overrideWebpackExclude'
 import { getDahliaConfig } from './getDahliaConfig'
+
+function resolve(path: string) {
+  return join(appDir, path)
+}
 
 export const customizeWebpack = () => {
   const webpackConfig = require(webpackConfigPath)
@@ -16,6 +22,17 @@ export const customizeWebpack = () => {
       styleComponents(),
       styledJsx(),
       overrideWebpackExclude(),
+      alias({
+        '@utils': resolve('utils/'),
+        '@stores': resolve('stores/'),
+        '@components': resolve('components/'),
+        '@layouts': resolve('layouts/'),
+        '@pages': resolve('pages/'),
+        '@modals': resolve('modals/'),
+        '@config': resolve('config/'),
+        '@interfaces': resolve('interfaces/'),
+        '@locales': resolve('locales/'),
+      }),
     )
 
     const dahliaConfig = getDahliaConfig()
