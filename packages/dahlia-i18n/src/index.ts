edd7e6e3c60efaa42c35bab686a6993a7c17c1db
forsigner <forsigner@gmail.com>
2019-04-08 15:53:10 +0800

    create dahlia-i18n

---
+++ packages/dahlia-i18n/src/index.ts
@@ -0,0 +1,29 @@
+import { I18n } from './typings'
+import get from 'lodash.get'
+
+const LOCALE_KEY = '__locale__'
+
+// TODO: 需重构
+const handler = {
+  get(_: any, name: string) {
+    const locale = window[LOCALE_KEY]
+    return get(locale, name)
+  },
+}
+
+const i18n: I18n = new Proxy({}, handler)
+
+function t(key: string) {
+  const locale = window[LOCALE_KEY]
+  return get(locale, key) || key
+}
+
+function getLocale() {
+  return localStorage.getItem('__lang__') || 'cn'
+}
+
+function setLocale(lang: string) {
+  localStorage.setItem('__lang__', lang)
+}
+
+export { i18n, t, getLocale, setLocale, I18n }
