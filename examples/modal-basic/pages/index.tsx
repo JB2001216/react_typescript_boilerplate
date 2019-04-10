f24dfaf4afd8d11fd94169746792e090b8033bf8
JB2001216 <zhangxing2002611@gmail.com>
2019-04-10 20:44:36 +0800

    examples: improve modal-basic

--- examples/modal-basic/pages/index.tsx
+++ examples/modal-basic/pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { Button } from 'antd'
 import { modalStore } from 'dahlia/modal'
-
-import 'antd/dist/antd.css'
 
 function open() {
   modalStore.open('About')
-  console.log(modalStore)
 }
 
 export default () => (
   <div>
     Hi, Dahlia
     <div>
-      <Button onClick={open}>Open</Button>
+      <button onClick={open}>Open</button>
     </div>
   </div>
 )
