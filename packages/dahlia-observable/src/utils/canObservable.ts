8e88bd9506702c45cd21f149b2cf33806f8aab74
forsigner <forsigner@gmail.com>
2019-04-10 20:07:48 +0800

    dahlia-observable: handle null object

--- packages/dahlia-observable/src/utils/canObservable.ts
+++ packages/dahlia-observable/src/utils/canObservable.ts
@@ -1,6 +1,7 @@
 import { globalState } from '../core/globalState'
 
-export function canObservable(obj: any): boolean {
+export function canObservable(obj: any = {}): boolean {
+  if (obj === null) return false
   if (typeof obj === 'function') return false
   if (globalState.raws.has(obj)) return false
   return true
