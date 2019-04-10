91f362814fd595e25293252b0e8a7fd178da3764
JB2001216 <zhangxing2002611@gmail.com>
2019-04-10 20:18:13 +0800

    dahlia-modal: use default modal container

--- packages/dahlia-modal/src/Modals.tsx
+++ packages/dahlia-modal/src/Modals.tsx
@@ -1,6 +1,6 @@
 import React, { ComponentType, Fragment } from 'react'
-import { Modal } from 'antd'
 import { observe } from 'dahlia-store'
+import DefaultModalContainer from './DefaultModalContainer'
 import { modalStore } from './modalStore'
 import { ModalConfig } from './typings'
 
@@ -10,22 +10,19 @@ const handleCancel = (name: string) => {
 
 const isVisible = (name: string) => {
   const { modals } = modalStore
-  return modals[name] && modals[name].isOpen
+  return modals[name] && modals[name].visible
 }
 
 export const Modals: ComponentType<{ config: ModalConfig }> = observe<{ config: ModalConfig }>(
   ({ config }) => {
     if (!config) return null
+    const { ModalContainer } = modalStore
+    const Modal = ModalContainer || DefaultModalContainer
     return (
       <Fragment>
-        {config.map(item => {
-          const { name } = item
-          const Current = item.component
-          const props = {} as any
-          if (Current.Title) props.title = <Current.Title />
-          if (Current.Footer) props.footer = <Current.Footer />
-          if (Current.onOk) props.onOk = Current.onOk
-          if (Current.onCancel) props.onCancel = Current.onCancel
+        {config.map(({ name, component }) => {
+          const Current = component
+          const props = Current.modalProps || {}
           return (
             <Modal
               visible={isVisible(name)}
