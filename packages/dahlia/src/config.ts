545f25f7413085222f09f587905ae69a738528f0
forsigner <forsigner@gmail.com>
2019-04-16 16:28:40 +0800

    dahlia: use rest and graphql

--- packages/dahlia/src/config.ts
+++ packages/dahlia/src/config.ts
@@ -1,18 +1,12 @@
 import { ComponentType } from 'react'
 import { Routes } from 'dahlia-router'
 import { ModalConfig } from 'dahlia-modal'
-import { Interceptor } from 'dahlia-http'
 
 export interface Config {
   routes: Routes
   modals: ModalConfig
-  graphql?: {
-    endpoint: string
-  }
-  rest?: {
-    endpoint: string
-    interceptor?: Interceptor
-  }
+  graphql?: any // TODO:
+  rest?: any // TODO:
   root: string
   app?: ComponentType<any>
 }
