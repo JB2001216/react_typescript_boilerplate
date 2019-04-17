27cb167f582bc37cbe1b372dc7ac5841aee38f13
forsigner <forsigner@gmail.com>
2019-04-17 12:52:34 +0800

    dahlia: update config

--- packages/dahlia/src/config.ts
+++ packages/dahlia/src/config.ts
@@ -1,12 +1,14 @@
 import { ComponentType } from 'react'
 import { Routes } from 'dahlia-router'
 import { ModalConfig } from 'dahlia-modal'
+import { RestConfig } from 'dahlia-rest'
+import { GraphqlConfig } from 'dahlia-graphql'
 
 export interface Config {
   routes: Routes
   modals: ModalConfig
-  graphql?: any // TODO:
-  rest?: any // TODO:
+  graphql?: GraphqlConfig
+  rest?: RestConfig
   root: string
   app?: ComponentType<any>
 }
