67b591fac98132446ae48d888cfec910554678cc
JB2001216 <zhangxing2002611@gmail.com>
2019-04-17 11:50:11 +0800

    examples: create rest examples

---
+++ examples/rest-useFetch/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useFetch } from 'dahlia/rest'
+
+export default () => {
+  const { loading, data, error } = useFetch('/todos/1')
+
+  if (loading) return <div>loading....</div>
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  return <pre>{JSON.stringify(data, null, 2)}</pre>
+}
---
+++ examples/rest-useUpdate/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useUpdate } from 'dahlia/rest'
+
+export default () => {
+  const [addTodo, { loading, data, error }] = useUpdate('/todos')
+
+  return (
+    <div className="App">
+      <button onClick={() => addTodo({ title: 'new TODO' })}>
+        {loading === undefined && 'Add Todo'}
+        {loading !== undefined && (loading ? 'loading...' : ' Added')}
+      </button>
+
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
+
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  )
+}
