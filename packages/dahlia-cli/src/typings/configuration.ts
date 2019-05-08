8b570bd706e0fbefee4a6fd54ecc668c2f0b6817
forsigner <forsigner@gmail.com>
2019-05-08 09:55:48 +0800

    dahlia-cli: support plugins

--- packages/dahlia-cli/src/typings/configuration.ts
+++ packages/dahlia-cli/src/typings/configuration.ts
@@ -1,10 +1,14 @@
 import { Configuration as WebpackConfig } from 'webpack'
 import { Configuration as ServerConfig } from 'webpack-dev-server'
 
-export interface Configuration {
-  title?: string
-  buildDir?: string
-  webpack?: (config: WebpackConfig, env: string) => WebpackConfig
+interface Plugin {
+  webpack?: (config: WebpackConfig, env?: string) => WebpackConfig
   // TODO: configFunction
   devServer?: (configFunction: any, env?: string) => ServerConfig
+}
+
+export interface Configuration extends Plugin {
+  title?: string
+  buildDir?: string
+  plugins?: Plugin[]
 }
