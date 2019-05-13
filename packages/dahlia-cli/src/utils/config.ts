584f2a6fb34fa562b672df642f1c3b3cd127e62e
JB2001216 <zhangxing2002611@gmail.com>
2019-05-13 16:32:40 +0800

    feat: support init dahlia-admin

--- packages/dahlia-cli/src/utils/config.ts
+++ packages/dahlia-cli/src/utils/config.ts
@@ -1,36 +1,4 @@
-export const deps = [
-  '@types/node',
-  '@types/jest',
-  '@types/react',
-  '@types/react-dom',
-  'dahlia',
-  'dahlia-cli',
-  'react',
-  'react-dom',
-  'typescript',
-]
 
-export const pkg = {
-  version: '0.1.0',
-  private: true,
-  scripts: {
-    start: 'dh start ',
-    build: 'dh build',
-  },
-  eslintConfig: {
-    extends: 'react-app',
-  },
-  browserslist: {
-    production: ['>0.2%', 'not dead', 'not op_mini all'],
-    development: [
-      'last 1 chrome version',
-      'last 1 firefox version',
-      'last 1 safari version',
-    ],
-  },
-}
-
-// TODO: check files
 export const filesToCheck = [
   'package.json',
   'public',
--- packages/dahlia-cli/src/utils/createPkgFile.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import fs from 'fs-extra'
-
-import { pkg } from './config'
-
-export function createPkgFile(root: string, appName: string) {
-  const pkgPath = path.join(root, 'package.json')
-  fs.writeJsonSync(pkgPath, { name: appName, ...pkg }, { spaces: 2 })
-}
