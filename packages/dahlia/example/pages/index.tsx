aabf2b3cec0ba327e173910455b386e0e4a1b970
JB2001216 <zhangxing2002611@gmail.com>
2019-05-16 10:09:34 +0800

    dahlia: update example

--- packages/dahlia/example/pages/index.tsx
+++ packages/dahlia/example/pages/index.tsx
@@ -3,7 +3,7 @@ import { modalStore } from 'dahlia/modal'
 
 export default () => (
   <div>
-    Hi, Dahlia
+    Hi, Dahlia!!!!
     <button onClick={() => modalStore.open('user')}>open</button>
   </div>
 )
