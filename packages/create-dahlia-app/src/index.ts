4fa772dd3da83ec6940586b07626ee5a12517ba5
JB2001216 <zhangxing2002611@gmail.com>
2019-06-17 15:53:35 +0800

    create-dahlia-app: created

---
+++ packages/create-dahlia-app/src/index.ts
@@ -0,0 +1,55 @@
+import { Command, flags } from '@oclif/command'
+import chalk from 'chalk'
+import path from 'path'
+import yosay from 'yosay'
+
+import { createAppDir } from './utils/createAppDir'
+import { checkAppDir } from './utils/checkAppDir'
+import { getProjectType } from './utils/getProjectType'
+import { createApp } from './utils/createApp'
+import { setAppName } from './utils/setAppName'
+import { install } from './utils/install'
+import { showTips } from './utils/showTips'
+
+const { green, yellow } = chalk
+
+class CreateDahliaApp extends Command {
+  static description = 'describe the command here'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+  }
+
+  static args = [{ name: 'appName' }]
+
+  async run() {
+    const { args } = this.parse(CreateDahliaApp)
+    const appName: string = args.appName
+    if (!appName) {
+      return this.log(
+        yellow('required project name, eg: '),
+        green('create-dahlia-app myapp'),
+      )
+    }
+    const root = path.resolve(appName)
+
+    console.log(yosay('您正在初始化 dahlia 项目...'))
+
+    try {
+      const projectType = await getProjectType()
+      const type = projectType === 'dahlia-admin' ? 'admin' : 'basic'
+      createAppDir(root)
+      checkAppDir(root, appName)
+      await createApp(root, type)
+      setAppName(root, appName)
+      await install(root)
+      showTips(root, appName)
+    } catch (error) {
+      this.log(error)
+    }
+  }
+}
+
+export = CreateDahliaApp
