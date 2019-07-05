87fc3fcdc9cc864e435a96c1240ca3f3cdec00b0
JB2001216 <zhangxing2002611@gmail.com>
2019-07-06 01:51:23 +0800

    dahlia-graphql: support request interceptor

--- packages/dahlia-graphql/src/query.ts
+++ packages/dahlia-graphql/src/query.ts
@@ -1,18 +1,39 @@
 import { GraphQLClient } from 'dahlia-graphql-client'
 import { graphqlConfig } from './config'
 import { Options, Variables } from './types'
+import { Interceptor } from './types'
 
 export const query = async <T = any>(input: string, options?: Options) => {
+  let opt: any
+  let interceptor = {} as Interceptor
+  const { endpoint, interceptor: configInterceptors } = graphqlConfig
+
   let variables: Variables = {}
   let headers: any = {}
-  const endpoint = graphqlConfig.endpoint
   if (options) {
     variables = options.variables || {}
     headers = options.headers || {}
   }
 
+  if (configInterceptors) interceptor = configInterceptors
+
+  if (interceptor.requests) {
+    interceptor.requests.forEach(item => {
+      // TODO:
+      opt = options
+        ? options
+        : {
+            headers: {},
+          }
+      opt = {
+        ...opt,
+        ...item(opt as any),
+      }
+    })
+  }
+
   try {
-    const client = new GraphQLClient({ endpoint, headers })
+    const client = new GraphQLClient({ endpoint, headers: { ...headers, ...opt.headers } })
     const data = await client.query<T>(input, variables)
     return data
   } catch (error) {
