f301db2979a452c838e24be031e71e51bbbb968d
JB2001216 <zhangxing2002611@gmail.com>
2019-08-02 11:01:30 +0800

    chore: rm no use code

--- packages/dahlia-graphql/src/use-query.ts
+++ packages/dahlia-graphql/src/use-query.ts
@@ -18,7 +18,6 @@ export function useQuery<T = any>(input: string, options?: Options) {
   const fetchData = async (opt: Options = {}) => {
     setState(prev => ({ ...prev, loading: true }))
     try {
-      console.log('options---------:', opt)
       const data = await query<T>(input, opt || {})
       if (!unmounted) setState(prev => ({ ...prev, loading: false, data }))
       return data
